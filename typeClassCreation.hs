77589405d3fe2a0a1bbb0b7d68ffd5d9d05a5263
Grant Mercer <Gmercer015@gmail.com>
2013-07-23 19:10:10 -0700

    added files and data

--- typeClassCreation.hs
+++ typeClassCreation.hs
@@ -2,10 +2,29 @@ data Point = Point Float Float deriving (Show)
 
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
+--surface area of object
 surface :: Shape -> Float
 surface (Circle _ r) = pi * r ^ 2
 surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
+--move a shape at x,y by a,b
 nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b) ) 
+
+--creates circle with default location at 0,0
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+--simple functions that use a base shape value
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
+
+--first name, last name, age, height, phone number, ice-cream flavor
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String
+                     } deriving (Show)
